44b2a53f8a91002dfda43c4f3d80ad116656e011
Smicardxz <benardetienne@outlook.com>
2025-05-23 13:20:11 +0200

    remove_login_register_from_landing_page

--- streamer-notifier/src/App.tsx
+++ streamer-notifier/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './styles/theme';
@@ -10,6 +10,26 @@ import Home from './components/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/NavBar';
 import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
+import LandingPage from './components/Landing';
+
+const AppContent: React.FC = () => {
+  const location = useLocation();
+  const isLandingPage = location.pathname === '/';
+
+  return (
+    <>
+      {!isLandingPage && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<ProtectedRoute />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
+};
 
 const App: React.FC = () => {
   return (
@@ -17,15 +37,7 @@ const App: React.FC = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<ProtectedRoute />}>
-              <Route index element={<Home />} />
-            </Route>
-            <Route path="/" element={<Navigate to="/login" />} />
-          </Routes>
+          <AppContent />
         </Router>
       </ThemeProvider>
     </AuthProvider>
